eb5072feb7e5b21860f7ee65a3d1f5d1b4e0e779
ByFor4 <by.for4@gmail.com>
2024-05-10 15:33:23 +0300

    12
    12

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-test
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "127.0.0.1",
+    port = "3306",
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
